b63856ca68eaae2a1277d6bd794d99ed7c988262
Mike Ebinum <mebinum@gmail.com>
2013-06-01 13:31:52 +1000

    added facetview dependencies and code added unlocked logo added gitignore

--- js/code.js
+++ js/code.js
@@ -2,6 +2,30 @@
 $(document).ready(function(){
    // cache the window object
 
+   //setup the facet view
+   $('.facet-view-simple').facetview({
+    search_url: 'http://bibsoup.net/query?',
+    search_index: 'elasticsearch',
+    facets: [
+        {'field':'year.exact', 'display': 'year'}, 
+        {'field':'publisher.exact', 'display': 'publisher'}
+    ],
+    paging: {
+      from: 0,
+      size: 10
+    }
+  });
+
+   // set up form
+  $('.demo-form').submit(function(e) {
+    e.preventDefault();
+    var $form = $(e.target);
+    var _data = {};
+    $.each($form.serializeArray(), function(idx, item) {
+      _data[item.name] = item.value;
+    });
+    $('.facet-view-here').facetview(_data);
+  });
    
 }); // close out script
 /* Create HTML5 element for IE */
